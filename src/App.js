5f23abb6293ec9315b56ff0e0b07b0143f97fb6b
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-24 20:45:11 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router , Route} from 'react-router-dom'
+import { Link } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Testing</h1>
+     <Router>
+       <nav style={{display:"flex"}}>
+         <div style={{margin:"auto"}}>
+           <Link to="/home">Home</Link>
+           <Link to="/about">About</Link>
+           <Link to="/contact">Contact</Link>
+         </div>
+         
+
+       </nav>
+     <Route Route path="/home" exact render={props => (
+        
+        <h1>Home</h1>
+        
+      
+    )} />
+
+    <Route Route path="/about" exact render={props => (
+        
+        <h1>About</h1>
+        
+      
+    )} />
+
+    <Route Route path="/contact" exact render={props => (
+        
+        <h1>Contact</h1>
+        
+      
+    )} />
+     </Router>
     </div>
   );
 }
