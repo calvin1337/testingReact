c78f33fa4cfdc13adfc808e395c599edd3444d78
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-26 19:41:49 +0100

    css

--- src/Components/Contact/ContactForm/ContactForm.js
+++ src/Components/Contact/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Col, Row } from "react-bootstrap"
+import "./contactForm.css"
 
 export class ContactForm extends Component {
     render() {
@@ -14,14 +15,49 @@ export class ContactForm extends Component {
                              Write A Message
                             </h3>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eveniet illo corrupti sint explicabo error impedit dolorem dolore architecto temporibus sunt fuga nam alias obcaecati, maiores quos unde? Sit, ex!</p>
+                        <p>If you have any question or inquires don't hesitate to contact us we are incredibly responsive to your requests and value your questions and we'll get back to you right away.</p>
                         <div className="contactImgContainer">
                         <img src="https://calvin1337.github.io/clockwise/images/logo2.png" alt="clockwise logo" />
                         </div>
                     </div>
                 </Col>
                 <Col lg="7">
-                
+                    <div className="contactFormContainer">
+                        <form action="#" className="contactForm">
+                            <Row>
+                                <Col lg="6">
+                                    <div style={{marginTop:"0px"}} className="inputBox">
+                                    <input type="text" placeholder="Your name" />
+                                    </div>
+                                </Col>
+                                <Col lg="6"> 
+                                    <div style={{marginTop:"0px"}} className="inputBox">
+                                    <input type="email" placeholder="Email Address" />
+                                    </div>
+                                </Col>
+                                <Col lg="6">
+                                    <div className="inputBox">
+                                    <input type="text" placeholder="Phone Number" />
+                                    </div>
+                                </Col>
+                                <Col lg="6">
+                                    <div className="inputBox">
+                                    <input type="text" placeholder="Subject" />
+                                    </div>
+                                </Col>
+                                <Col lg="12">
+                                    <div class="inputBox">
+                                    <textarea placeholder="Write Message"></textarea>
+                                    </div>
+                                </Col>
+                                <Col className="text-right">
+                                    <div class="inputBox">
+                                    <button class="SubmitBtn" type="submit">Submit</button>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </form>
+                    </div> 
                 </Col>
             </Row>
         )
