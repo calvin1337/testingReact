e2bd39d6d863580b6e1bb13bbf6a3402f96b7e4c
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-06-08 16:26:43 +0100

    privacy page

--- src/Containers/Privacy/Privacy.js
+++ src/Containers/Privacy/Privacy.js
@@ -4,16 +4,187 @@ import {Row, Container, Col} from "react-bootstrap"
 export class Privacy extends Component {
     render() {
         return (
-            <div style={{paddingTop:"100px"}}>
-                <Container style={{paddingTop:"100px"}}>
+            <div className="privacy" style={{paddingTop:"100px"}}>
+                <Container style={{paddingTop:"100px", paddingBottom:"100px"}}>
                     <Row>
                         <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Privacy Policy</h2>
+
+                         </div>
                             <p>This privacy policy is for the website www.clockwise-tech.com and served by AB Engineering (Laurencekirk) Ltd and governs the privacy of those who choose to use the site. This policy sets out the different areas where user privacy is concerned and outlines the obligations and requirements of the users, the website and website owners. Furthermore the way this website processes, stores and protects user data and information will also be detailed within this policy. </p>
                         </Col>
                         <Col md="12">
                         <div class="sectionTitle">
                         <h2>The Website</h2>
+
                          </div>
+                        <p>
+                            This website and it’s owners take a proactive approach to user privacy and ensure the necessary steps are taken to protect the privacy of its users throughout their visiting experience. This website complies to all UK national laws and requirements for user privacy. 
+                        </p>
+
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Use Of Cookies </h2>
+
+                         </div>
+                         <p>
+                         This website uses cookies to better the users experience while visiting the website. Where applicable this website uses a cookie control system allowing the user on their first visit to the website to allow or disallow the use of cookies on their device. 
+                         </p>
+                         <p>
+                         Cookies are small files stored on the users device that track, save and store information about the user’s interactions and usage of the website. This allows the website, through its server to provide the users with a tailored experience within this website.
+                         </p>
+                         <p>
+                         This website uses tracking software to monitor its visitors to better understand how they use it. This software is provided by Google Analytics which uses cookies to track visitor usage. The software will save a cookie to your computers hard drive in order to track and monitor your engagement and usage of the website, but will not store, save or collect personal information. You can read Google’s privacy policy here for further information [ http://www.google.com/privacy.html ]. 
+                         </p>
+                        <p>
+                        Other cookies may be stored to your computer’s hard drive by external vendors when this website uses referral programs, sponsored links or adverts. Such cookies are used for conversion and referral tracking and typically expire after 30 days, though some may take longer. No personal information is stored, saved or collected. 
+                        </p>
+                        </Col>
+                        <Col md="12">
+
+                        <div class="sectionTitle">
+                        <h2>External Links</h2>
+
+                         </div>
+                        <p>
+                        Although this website only looks to include quality, safe and relevant external links, users are advised adopt a policy of caution before clicking any external web links mentioned throughout this website. (External links are clickable text / banner / image links to other websites.) 
+                        </p>
+                        <p>
+                        The owners of this website cannot guarantee or verify the contents of any externally linked website despite their best efforts. Users should therefore note they click on external links at their own risk and this website and its owners cannot be held liable for any damages or implications caused by visiting any external links mentioned. 
+                        </p>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Social Media Platforms</h2>
+
+                         </div>
+                         <p>
+                         Communication, engagement and actions taken through external social media platforms that this website and its owners participate on are custom to the terms and conditions as well as the privacy policies held with each social media platform respectively. 
+                         </p>
+                         <p>
+                         Users are advised to use social media platforms wisely and communicate / engage upon them with due care and caution in regard to their own privacy and personal details. This website nor its owners will ever ask for personal or sensitive information through social media platforms and encourage users wishing to discuss sensitive details to contact them through primary communication channels such as by telephone or email. 
+                         </p>
+                         <p>
+                         This website may use social sharing buttons which help share web content directly from web pages to the social media platform in question. Users are advised before using such social sharing buttons that they do so at their own discretion and note that the social media platform may track and save your request to share a web page respectively through your social media platform account. 
+                         </p>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Shortened Links in Social Media</h2>
+
+                         </div>
+                         <p>
+                         This website and its owners through their social media platform accounts may share web links to relevant web pages. By default, some social media platforms shorten lengthy URL’s. 
+                         </p>
+                         <p>
+                         Users are advised to take caution and good judgement before clicking any shortened URL’s published on social media platforms by this website and its owners. Despite the best efforts to ensure only genuine URL’s are published many social media platforms are prone to spam and hacking and therefore this website and its owners cannot be held liable for any damages or implications caused by visiting any shortened links. 
+                         </p>
+                        </Col>
+                        <Col>
+                        <div class="sectionTitle">
+                        <h2>Contact And Communication</h2>
+
+                         </div>
+                         <p>
+                         Users contacting this website and/or its owners agree for the owners to process their information in accordance with the terms and conditions outlined in this policy.
+                         </p>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Collection And Use of Personal Information</h2>
+
+                         </div>
+                         <p>
+                         If you use the website, personal information that identifies you as an individual or that relates to an identifiable person (“Personal Information”) may be collected and processed by Expro employees. In general, we collect and process Personal Information when you knowingly provide it to us, such as but not limited to: 
+                         </p>
+                         <ul>
+                             <li>
+                                 Name
+                             </li>
+                             <li>
+                                 Email Address
+                             </li>
+                             <li>
+                                 Company
+                             </li>
+                             <li>
+                                 Telephone Number 
+                             </li>
+                             <li>
+                                 IP Address
+                             </li>
+                         </ul>
+
+                         <p>
+                         We may need to collect and process these types of Personal Information in order to provide information to you, such as.
+                         </p>
+
+                        <ul>
+                            <li>To respond to your inquiries and fulfil your requests, such as to send you newsletters.</li>
+                            <li>To send administrative information to you, for example, information regarding services and changes to our terms, conditions and policies.</li>
+                            <li>To send you marketing and informational communications such as the latest industry publications and resources on topics you have selected, where you have chosen to receive such communications. Additionally, we may automatically collect or process Personal Information (such as your Internet domain or IP address) as part of the operation of the website or during the course of your activities.</li>
+                        </ul>
+
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Data Retention</h2>
+
+                         </div>
+                         <p>
+                         We will not retain your Personal Information longer than is necessary and permitted in light of the purpose(s) for which it was obtained. The criteria used to determine our retention periods include: the length of time we have an ongoing relationship with you; whether there is a legal obligation to which we are subject; or whether retention is advisable in light of our legal position (such as in regard to applicable statutes of limitations, litigation or regulatory investigations). 
+                         </p>
+
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Third Parties</h2>
+
+                         </div>
+                         <p>
+                         We may also disclose your Personal Information to our third party service providers who provide services such as website hosting, data analysis, information technology and related infrastructure provision, email delivery, and other services.
+                         </p>
+                         <p>
+                         We will not knowingly sell, trade, or disclose your Personal Information to third parties that want to market products to you. 
+                         </p>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Sensitive Information</h2>
+
+                         </div>
+                         <p>
+                         We do not knowingly collect sensitive Personal Information (such as, social security numbers, information related to racial or ethnic origin, political opinions, religion or other beliefs, health, biometrics or genetic characteristics, criminal background or trade union membership) from you via the website. Please do not send us, or disclose, any sensitive Personal Information on or through the website. 
+                         </p>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Other information</h2>
+
+                         </div>
+                         <p> We may also collect other information, such as: </p>
+                         <ul>
+                             <li>
+                             Browser and device information
+                             </li>
+                             <li>
+                             Information collected through cookies, pixel tags and other technologies
+                             </li>
+                             <li>
+                             Demographic information and other information provided by you that does not reveal your specific identity We and our service providers may collect other information in a variety of ways, including: Through your browser or device: Certain information is collected by most browsers or automatically through your device, such as your Media Access Control (MAC) address, computer type (Windows or Mac), screen resolution, operating system name and version, device manufacturer and model, language, Internet browser type and version and the name and version of the services you are using. We use this information to ensure that the services function properly. 
+                             </li>
+                         </ul>
+                        </Col>
+                        <Col md="12">
+                        <div class="sectionTitle">
+                        <h2>Contact us</h2>
+
+                         </div>
+                         <p>
+                         If you have any questions about this privacy statement, the practices of this site, or your dealings with this web site, you can contact us at info@clockwise-tech.com 
+                         </p>
                         </Col>
                     </Row>
                 </Container>
