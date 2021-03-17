343d677b496a25cbe38b35cbcb5e393af1ad6acb
calvin1337 <calvindonaldson1997313@gmail.com>
2021-03-17 22:22:44 +0000

    projects new

---
+++ build/precache-manifest.a0e688db36668fd2348484da0b6bde57.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132711a22dc77563424a1bdeb54429d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cf884c6a21916fef9c5",
+    "url": "/static/css/2.1d143842.chunk.css"
+  },
+  {
+    "revision": "2b56fad3728c421efd11",
+    "url": "/static/css/main.37342c6b.chunk.css"
+  },
+  {
+    "revision": "9cf884c6a21916fef9c5",
+    "url": "/static/js/2.37fd0b49.chunk.js"
+  },
+  {
+    "revision": "a90a0449d8ac6943f0cd62c0929c2a63",
+    "url": "/static/js/2.37fd0b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b56fad3728c421efd11",
+    "url": "/static/js/main.a8049ee6.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4244b4e881b63b87fa52860fb16584d2",
+    "url": "/static/media/TDUFrame.4244b4e8.jpg"
+  },
+  {
+    "revision": "9d77748339e59c7a6e69f0539534a47d",
+    "url": "/static/media/aluminumwelding.9d777483.jpg"
+  },
+  {
+    "revision": "930bd46efda81a01b35ff2c97fa2beab",
+    "url": "/static/media/assembly.930bd46e.jpg"
+  },
+  {
+    "revision": "0d978235b62a2c40dc200d345ca4fefe",
+    "url": "/static/media/cablecutter.0d978235.jpg"
+  },
+  {
+    "revision": "e56c20b6835d8dbf2fc8422a44b63636",
+    "url": "/static/media/debricap.e56c20b6.jpg"
+  },
+  {
+    "revision": "318ad2b679ef2cda9a8be240ef0cf918",
+    "url": "/static/media/designandenginering.318ad2b6.jpg"
+  },
+  {
+    "revision": "96e39ae74a7bb28b6a6a30bac52b755a",
+    "url": "/static/media/designandenginering1.96e39ae7.jpg"
+  },
+  {
+    "revision": "34724baaf2c85d891b149e75a2c11e19",
+    "url": "/static/media/diverskid.34724baa.jpg"
+  },
+  {
+    "revision": "0f01c36158ec8f5ca3722042e2e3c44f",
+    "url": "/static/media/exoticwelding.0f01c361.jpg"
+  },
+  {
+    "revision": "f8c0525f7cf633bbb0dd1517ebaf0dfd",
+    "url": "/static/media/fabrication.f8c0525f.jpeg"
+  },
+  {
+    "revision": "49269ed68b055c86cab01f0a6b927496",
+    "url": "/static/media/hpuskid.49269ed6.jpg"
+  },
+  {
+    "revision": "0b4a2308497d4ab6e64d24bf583c0870",
+    "url": "/static/media/hydrolicpress.0b4a2308.jpg"
+  },
+  {
+    "revision": "8fd6fb22242ce1e1e7669cfeea36f248",
+    "url": "/static/media/logowhite.8fd6fb22.png"
+  },
+  {
+    "revision": "81624d5eaa46ca18cd9980f8f0c1daaf",
+    "url": "/static/media/machining.81624d5e.jpg"
+  },
+  {
+    "revision": "97aaffd42794bf7a564c4d0d78d29b28",
+    "url": "/static/media/oceaneering.97aaffd4.png"
+  },
+  {
+    "revision": "e6304e248335630347ff7c55415114fe",
+    "url": "/static/media/rovskid.e6304e24.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "8291be084fd7b6c692ded779564046cd",
+    "url": "/static/media/spacerframe.8291be08.jpg"
+  },
+  {
+    "revision": "10aee7a54765cc85bd2ecf1f0e389736",
+    "url": "/static/media/toolingbasket.10aee7a5.jpg"
+  },
+  {
+    "revision": "9ab104c69bdc458dc8466e1ab5cea979",
+    "url": "/static/media/weldinghp.9ab104c6.jpg"
+  },
+  {
+    "revision": "8c758cfcf2005c7dc0320e7cca9a6ba2",
+    "url": "/static/media/weldinghp2.8c758cfc.jpeg"
+  }
+]);
